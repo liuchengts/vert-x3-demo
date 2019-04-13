7eadb3eb57c237badc4a3fdaab6ea0dc00b1960d
刘成 <790809568@qq.com>
2019-04-13 11:28:47 +0800

    测试

--- src/main/java/com/lc/study/VertxApplication.kt
+++ src/main/java/com/lc/study/VertxApplication.kt
@@ -5,18 +5,29 @@ import io.vertx.kotlin.core.http.httpServerOptionsOf
 
 class VertxApplication {
 
-
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-
+            VertxApplication().server()
         }
     }
 
     fun server() {
-        val vertx= Vertx.vertx()
-        var options = httpServerOptionsOf(
-                maxWebsocketFrameSize = 1000000)
+        val vertx = Vertx.vertx()
+        val options = httpServerOptionsOf(
+                maxWebsocketFrameSize = 1000000,//设置最大数据帧大小
+                logActivity = true)//记录网络活动
         var server = vertx.createHttpServer(options)
+                .requestHandler {
+                    val path = it.path()
+                    val params = it.params()
+                    it.response().end("收到你的消息了:$path$params", "UTF-8")
+                }.listen(8080) {
+                    if (it.succeeded()) {
+                        println("Server is now listening!")
+                    } else {
+                        println("Failed to bind!")
+                    }
+                }
     }
 }
